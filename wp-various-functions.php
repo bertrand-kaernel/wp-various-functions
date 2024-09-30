a16d273a965eea96dbbd4dd70486e19e7ea88ec8
Bertrand Kaernel <135952984+bertrand-kaernel@users.noreply.github.com>
2024-09-30 15:41:16 +0200

    Update wp-various-functions.php

--- wp-various-functions.php
+++ wp-various-functions.php
@@ -8,8 +8,8 @@ function bk_include_svg($svg_file)
 {
     $path_to_svg = file_get_contents(get_template_directory() . '/bk-folder/images/' . $svg_file);
     if ( !empty($path_to_svg) ) {
-        echo $path_to_svg;
+       return $path_to_svg;
     } else {
-        echo '&varnothing;';
+        return  '&varnothing;';
     }
 }
