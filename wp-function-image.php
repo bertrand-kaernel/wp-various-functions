2e0c67c18604418201656279d833a6f89c42cc48
Bertrand Kaernel <135952984+bertrand-kaernel@users.noreply.github.com>
2024-09-30 17:19:16 +0200

    Update wp-function-image.php

--- wp-function-image.php
+++ wp-function-image.php
@@ -17,7 +17,7 @@ update_option( 'large_size_h', 1280); // Set Thumbnail height - default 150
 update_option( 'large_crop', 1 );     // Set Crop mode - 0 Soft, 1 Hard
 
 /// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-/// ADD MY SIZES
+/// ADD MY SIZES - SIZE + RATIO
 /// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 add_action( 'after_setup_theme', 'wpdocs_theme_setup' );
