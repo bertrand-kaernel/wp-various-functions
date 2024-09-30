a79b66770149081f7a84ea1a8972f4ae699e62e0
Bertrand Kaernel <135952984+bertrand-kaernel@users.noreply.github.com>
2024-09-30 17:18:44 +0200

    Update wp-function-image.php

--- wp-function-image.php
+++ wp-function-image.php
@@ -1,5 +1,9 @@
 <?php 
 
+/// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/// WP FUNCTIONS FOR IMAGES
+/// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 update_option( 'thumbnail_size_w', 525 ); // Set Thumbnail width - default 150
 update_option( 'thumbnail_size_h', 525 ); // Set Thumbnail height - default 150
 update_option( 'thumbnail_crop', 1 );     // Set Crop mode - 0 Soft, 1 Hard
@@ -11,3 +15,35 @@ update_option( 'medium_crop', 1 );     // Set Crop mode - 0 Soft, 1 Hard
 update_option( 'large_size_w', 1920 ); // Set Thumbnail width - default 150
 update_option( 'large_size_h', 1280); // Set Thumbnail height - default 150
 update_option( 'large_crop', 1 );     // Set Crop mode - 0 Soft, 1 Hard
+
+/// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/// ADD MY SIZES
+/// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+add_action( 'after_setup_theme', 'wpdocs_theme_setup' );
+function wpdocs_theme_setup() {
+  
+    add_image_size( 'L-169', 1920, 1080, true ); 
+    add_image_size( 'L-43', 1920, 1440, true ); 
+    add_image_size( 'M-169', 768, 432, true ); 
+    add_image_size( 'M-43', 768, 576, true );
+    add_image_size( 'S-43', 525, 319, true );
+}
+
+/// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/// SIZES DANS LE TABLEAU DE BORD
+/// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+add_filter('image_size_names_choose', 'add_custom_thumb');
+
+function add_custom_thumb($sizes) {
+    $addsizes = array(
+        "large-169"    => __( "1920 x 1080"),
+        "large-43"     => __( "1920 x 1440"),
+        "medium-169"   => __( "768 x 432"),
+        "medium-43"    => __( "768 x 576"),
+        "small-43"     => __( "525 x 319"),
+    );
+    $newsizes = array_merge($sizes, $addsizes);
+    return $newsizes;
+}
