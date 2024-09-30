314a1f433815dacbda281990c5111f4442f764ef
Bertrand Kaernel <135952984+bertrand-kaernel@users.noreply.github.com>
2024-09-30 15:32:07 +0200

    Create wp-various-functions.php

---
+++ wp-various-functions.php
@@ -0,0 +1,15 @@
+<?php 
+
+/// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/// INCLUDE SVG IN WORDPRESS
+/// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function bk_include_svg($svg_file)
+{
+    $path_to_svg = file_get_contents(get_template_directory() . '/bk-folder/images/' . $svg_file);
+    if ( !empty($path_to_svg) ) {
+        echo $path_to_svg;
+    } else {
+        echo '&varnothing;';
+    }
+}
