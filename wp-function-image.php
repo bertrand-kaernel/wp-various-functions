fb8cd51985aa79cea84019243ea62708cc2bf713
Bertrand Kaernel <135952984+bertrand-kaernel@users.noreply.github.com>
2024-09-30 17:12:04 +0200

    Create wp-function-image.php

---
+++ wp-function-image.php
@@ -0,0 +1,13 @@
+<?php 
+
+update_option( 'thumbnail_size_w', 525 ); // Set Thumbnail width - default 150
+update_option( 'thumbnail_size_h', 525 ); // Set Thumbnail height - default 150
+update_option( 'thumbnail_crop', 1 );     // Set Crop mode - 0 Soft, 1 Hard
+
+update_option( 'medium_size_w', 768 ); // Set Thumbnail width - default 150
+update_option( 'medium_size_h', 768 ); // Set Thumbnail height - default 150
+update_option( 'medium_crop', 1 );     // Set Crop mode - 0 Soft, 1 Hard
+
+update_option( 'large_size_w', 1920 ); // Set Thumbnail width - default 150
+update_option( 'large_size_h', 1280); // Set Thumbnail height - default 150
+update_option( 'large_crop', 1 );     // Set Crop mode - 0 Soft, 1 Hard
