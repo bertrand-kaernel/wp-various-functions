55e9d3b67bbf1c4805a5a55bffbfddab61649e13
Bertrand Kaernel <135952984+bertrand-kaernel@users.noreply.github.com>
2024-10-03 13:06:22 +0200

    Update wp-various-functions.php

--- wp-various-functions.php
+++ wp-various-functions.php
@@ -13,3 +13,31 @@ function bk_include_svg($svg_file)
         return  '&varnothing;';
     }
 }
+
+/// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/// PAGINATION FOR ARCHIVE AND PAGE TEMPLATE
+/// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function bk_archive_pagination($query = null) // Here the name of your query
+{
+  if ($query === null) { global $wp_query;
+    $query = $wp_query;
+  }
+  
+  $big = 999999999;
+
+  $pagination = paginate_links(array(
+    'base'      => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+    'format'    => '?paged=%#%',
+    'current'   => max(1, get_query_var('paged')),
+    'total'     => $query->max_num_pages,
+    'prev_text' => __('« Previous'),
+    'next_text' => __('Next »'),
+  ));
+
+  if ($pagination) {
+    return '<div class="archive-navigation">' . $pagination . '</div>';
+  } else {
+    return '<div class="archive-navigation">' . esc_attr('RIENICI') .' </div>';
+  }
+}
